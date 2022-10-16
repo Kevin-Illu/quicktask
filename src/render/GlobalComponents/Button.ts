4e68def4e6b52aba1efb91f9c96d8b522da77ad1
Kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2022-10-16 13:48:51 -0600

    lipieza xd

--- src/render/GlobalComponents/Button.ts
+++ src/render/GlobalComponents/Button.ts
@@ -1,53 +1,54 @@
-import {addStyles} from '../utils/add-styles.js'
+import { addStyles } from '../utils/add-styles.js'
 import { btnSettings } from '../types/index';
 
-
 class Button {
-	public button: HTMLButtonElement;
-	private _settings: btnSettings;
-	private _text: HTMLParagraphElement;
-	private _img: HTMLImageElement;
-	private _container: HTMLDivElement;
+  public button: HTMLButtonElement;
+  private _settings: btnSettings;
+  private _text: HTMLParagraphElement;
+  private _img: HTMLImageElement;
+  private _container: HTMLDivElement;
 
-	constructor({icon = null, text = null, func = null, styles = []}: btnSettings) {
-		this.button = document.createElement("button");
-		this._settings = {icon,text,func,styles}
-		this._text = document.createElement("p");
-		this._img = document.createElement("img"); 
-		this._container = document.createElement("div");
-		this.appendContent(this._settings);
-	};
+  constructor({ icon = null, text = null, func = null, styles = [] }: btnSettings) {
+    this.button = document.createElement("button");
+    this._settings = { icon, text, func, styles }
+    this._text = document.createElement("p");
+    this._img = document.createElement("img");
+    this._container = document.createElement("div");
+    this.appendContent(this._settings);
+  };
 
-	appendContent = (settings: btnSettings): void => {
-		const {icon,text,func,styles} = settings;
-
-		addStyles(this.button,styles);
-		if(func !== null) this.button.onclick = func;
-
-		if (!icon && !text) {
-			const message = `Provide a valid icon or text for the 
+  appendContent = (settings: btnSettings): void => {
+    const { icon, text, func, styles } = settings;
+    const errorMessage = `Provide a valid icon or text for the 
 			button if you provide both then the button will 
 			contain icon and text`
-			throw new Error(message);
-		} else if (icon && text) {
-			this._img.src = icon;
-			this._text.innerText = text;
 
-			this._container.appendChild(this._img);
-			this._container.appendChild(this._text);
-			this.button.appendChild(this._container);
-		} else if (!icon) {
-			this._text.innerText = text || 'no hay texto :/';
-			this.button.appendChild(this._text);
-		} else if (!text) {
-			this._img.src = icon;
-			this.button.appendChild(this._img);
-		}
-	}
+    addStyles(this.button, styles);
+    if (func !== null) this.button.onclick = func;
 
-	renameButton = (text: string) => {
-		this._text.innerText = text;
-	}
+    if (!icon && !text) {
+      throw new Error(errorMessage);
+    } else if (icon && text) {
+      this._img.src = icon;
+      this._text.innerText = text;
+
+      this._container.appendChild(this._img);
+      this._container.appendChild(this._text);
+      this.button.appendChild(this._container);
+    } else if (!icon) {
+      this._text.innerText = text || 'no hay texto :/';
+
+      this.button.appendChild(this._text);
+    } else if (!text) {
+      this._img.src = icon;
+
+      this.button.appendChild(this._img);
+    }
+  }
+
+  renameButton = (text: string) => {
+    this._text.innerText = text;
+  }
 };
 
 
--- src/render/index.ts
+++ src/render/index.ts
@@ -5,6 +5,7 @@ import ContainerApp from './Content/Layout/ContainerApp.js';
 import MainNavigation from './MainNavigation/Main-navigation-bar.js';
 import { btnClose, btnMinimize, btnMaximize } from './MainNavigation/traffic-lights.js';
 import TodoApp from './Content/Views/Todo/TodoApp.js'
+import { btnSettings } from './types/index.js';
 
 // window topBar
 const navigate = new MainNavigation(["navigation"], [btnMinimize, btnMaximize, btnClose]);
@@ -12,11 +13,11 @@ const Container = new ContainerApp();
 const Todo = new TodoApp(Container.containerApp);// apps
 
 // TODO: add a lenght of the todos in the button TASK
-let lengthTodo = `${Todo.getLenghtTodos()}`;
+// let lengthTodo = `${Todo.getLenghtTodos()}`;
 
-const btnTodoSettings = {
+const btnTodoSettings: btnSettings = {
   icon: './public/assets/checklist.svg',
-  text: `Task ${lengthTodo}`,
+  text: 'Task',
   func: Todo.displayTodos,
   styles: ['navbar-btn', 'navbar-btn__display-todos', 'btn'],
 }
@@ -26,13 +27,10 @@ const navBar = new NavBar();// navbar apps
 const apps = [btnDisplayTodo]
 navBar.addApplications(apps);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root: HTMLElement | null = document.getElementById("root");
-  if (!root) return;
-  root.appendChild(navigate.navigationBar);
-  // add navbar at the container app
-  Container.containerNavBar.appendChild(navBar.NavBarContent);
-  navigate.setTitle("TodoApp");
-  root.appendChild(Container.container);
-  Todo.displayTodos();
-})
+const root: HTMLElement | null = document.getElementById("root");
+root?.appendChild(navigate.navigationBar);
+// add navbar at the container app
+Container.containerNavBar.appendChild(navBar.NavBarContent);
+navigate.setTitle("TodoApp");
+root?.appendChild(Container.container);
+Todo.displayTodos();
