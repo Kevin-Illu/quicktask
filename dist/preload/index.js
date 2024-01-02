79cd475d2e0deabb901895a534281c46ccf46c2f
kevin-illu <kevinillu97@gmail.com>
2024-01-02 12:44:29 -0600

    fix(remove dist): remove dist directory, this directory is generated automatically

--- dist/preload/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('mainApi', {
-    trafficsLightsAction: (action) => ipcRenderer.send('btn-event', action),
-});
--- dist/render/utils/tools.js
+++
@@ -1,8 +0,0 @@
-export const RemoveChild = (parent) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-};
-export const addStyles = (element, styles) => {
-    styles.forEach((style) => element.classList.add(style));
-};
